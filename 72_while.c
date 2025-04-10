4f98d8221d9f07f6d3f08bd3e9b194d0fd8bac90
ChetanPatel1999 <chetan8085patel@gmail.com>
2025-04-10 18:10:19 +0530

    04/10

---
+++ 72_while.c
@@ -0,0 +1,10 @@
+// while loop example
+//code run infinite times
+#include <stdio.h>
+void main()
+{
+    while (12 > 8)
+    {
+        printf("hello world institute\n");
+    }
+}
---
+++ 73_while.c
@@ -0,0 +1,18 @@
+// wap to print a msg 10 times.
+#include <stdio.h>
+void main()
+{
+    int i;
+    i = 1;
+    while (i <= 5)
+    {
+        printf("hello world institute\n");
+        i++; // 4
+    }
+    i=1;
+    while(i<=7)
+    {
+       printf("hi krishna \n");
+       i++; 
+    }
+}
